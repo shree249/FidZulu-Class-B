03cd84ca053476d0c494f1a9727144f11b805ec6
Associate <Associate@roifmrwindt>
2023-10-11 00:46:01 +0530

    e2e done

--- toyService/spec/toys.spec.js
+++ toyService/spec/toys.spec.js
@@ -1,50 +1,58 @@
-// const { Router } = require('express'); // Import your route handler
-// const router = Router(); // Create an instance of your route handler
+const request = require('request');
+const app = require('../index');
+var http = require('http');
 
-// // Mock your route dependencies
-// const mockResponse = {
-//   json: jasmine.createSpy('json'),
-//   send: jasmine.createSpy('send'),
-//   status: jasmine.createSpy('status').and.returnValue(mockResponse),
-// };
-// const next = jasmine.createSpy('next');
-// const base_url = 'http://localhost:3033/';
-// const toys_url = base_url + 'toys/Raleigh';
-// const not_found_url = base_url + 'toys/';
+// Mock your route dependencies
 
+const base_url = 'http://localhost:3001/';
+const toys_url = base_url + 'toys/Raleigh';
+const not_found_url = base_url + 'toys/';
 
-// describe("Toys server  endpoint tests", function () {
+app.set('port', 3001);
+
+describe("Toys server  endpoint tests", function () {
+    let server;
+
+    beforeAll(() => {
+        server = http.createServer(app);
+        server.listen(3001);
+    });
+  
+    afterAll((done) => {
+        
+      server.close(done); // Shutdown the server after tests are complete
+    });
     
-//     describe("GET /toys/Raleigh", () => {
-//         it("returns status code 200",  (done) => {
-//             request.get(toys_url, (error, response, body) => {
-//                 expect(response.statusCode).toBe(200);
-//                 done();
-//             });
-//         });
-//         it("contains price", (done) => {
-//             request.get(toys_url, (error, response, body) => {
-//                 expect(body).toBeTruthy();
-//                 expect(body).toContain("prize");
-//                 done();
-//             });
-//         });
-//     });
-//     // test for wrong path and expect 404
-//     describe("GET /toys/", () => {
-//         it("returns status code 404",  (done) => {
-//             request.get(not_found_url, (error, response, body) => {
-//                 expect(response.statusCode).toBe(404);
-//                 done();
-//             });
-//         });
-//     });
-//     describe("GET /toys/China", () => {
-//         it("returns status code 404",  (done) => {
-//             request.get(not_found_url + "China", (error, response, body) => {
-//                 expect(response.statusCode).toBe(404);
-//                 done();
-//             });
-//         });
-//     });
-// });
+    describe("GET /toys/Raleigh", () => {
+        it("returns status code 200",  (done) => {
+            request.get(toys_url, (error, response, body) => {
+                expect(response.statusCode).toBe(200);
+                done();
+            });
+        });
+        it("contains price", (done) => {
+            request.get(toys_url, (error, response, body) => {
+                expect(body).toBeTruthy();
+                expect(body).toContain("prize");
+                done();
+            });
+        });
+    });
+    // test for wrong path and expect 404
+    describe("GET /toys/", () => {
+        it("returns status code 404",  (done) => {
+            request.get(not_found_url, (error, response, body) => {
+                expect(response.statusCode).toBe(404);
+                done();
+            });
+        });
+    });
+    describe("GET /toys/China", () => {
+        it("returns status code 404",  (done) => {
+            request.get(not_found_url + "China", (error, response, body) => {
+                expect(response.statusCode).toBe(404);
+                done();
+            });
+        });
+    });
+});
