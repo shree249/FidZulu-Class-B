3f36f6025d9009d9b3d1f4355ccb41f40f7f408c
Associate <Associate@roifmrwindt>
2023-10-10 16:37:51 +0530

    set up done

---
+++ bikeService/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const debug = require('debug')('foodService:server');
+
+// Define your routes and middleware here
+app.get('/', (req, res) => {
+    res.send('Hello, Express!');
+});
+
+// Set up any additional routes or middleware as needed
+
+module.exports = app;
